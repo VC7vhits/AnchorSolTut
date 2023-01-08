3f1b1a8e6d55d67409ec2b7076368d03e2e9d5a0
VC7vhits <vishalcpatel.vhits@gmail.com>
2023-01-08 15:06:29 +0530

    Added sol transfer code

--- tests/tut2.ts
+++ tests/tut2.ts
@@ -7,47 +7,75 @@ import { readFileSync } from 'fs';
 const log = console.log;
 
 describe("tut2", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
   const provider = anchor.getProvider();
-  let myKey = provider.publicKey;
-
-  let fileTxt = readFileSync("./_users/additions.json", { encoding: 'utf-8' });
-  let kepairJson = JSON.parse(fileTxt);
-  let buffers_8 = Uint8Array.from(kepairJson);
-  let account = anchor.web3.Keypair.fromSecretKey(buffers_8);
 
   const program = anchor.workspace.Tut2 as Program<Tut2>;
   const receiver = new anchor.web3.PublicKey("4MMr4CMFuyBmpbK3RvwdxaipMvkUY4Y25pKYgtEuMYjG");
 
-  it("init account", async () => {
-    let sign = program.methods.initialize().accounts({
-      user: provider.publicKey,
-      account: account.publicKey,
+  it("sol transfer through Our Contract", async () => {
+    let sign = await program.methods.solTransfer(new anchor.BN(1)).accounts({
+      sender: provider.publicKey,
+      receiver: receiver,
       systemProgram: anchor.web3.SystemProgram.programId,
     })
-      .signers([account])
       .rpc();
 
-    log("Tx Sign", sign);
-  })
+    console.log("Transaction Sign: ", sign);
 
-  it('Additions', async () => {
-    let sign = program.methods.add(1, 4).accounts({
-      account: account.publicKey,
+
+    //? Basically the .rpc() methdos hides much more things
+    //* here it ix means the instruction. => a Transaction is made by the instructions.
+    let ix = await program.methods.solTransfer(new anchor.BN(1)).accounts({
+      sender: provider.publicKey,
+      receiver: receiver,
+      systemProgram: anchor.web3.SystemProgram.programId,
     })
-      .signers([account])
-      .rpc();
+      .instruction();
+
+    let tx = new anchor.web3.Transaction();
+    tx.add(ix); //? Addding the transaction.
+
+    tx.feePayer = provider.publicKey; //? to execute the transaction in blockchain it's take fees
+    //? to execute the transaction, here it's require the feePayer.
 
 
-    log("Tx Sing: ", sign);
+    //? To send transaction it's require the recentBlockhash,
+    //? may be it's user the find out the time of transaction when it was sended.
+    const blockHashResult = await provider.connection.getLatestBlockhash()
+    tx.recentBlockhash = blockHashResult.blockhash
+
+
+    //? every transaction is signed by at least one keypair.
+    //? because to make the trasaction it's require to the feesPayer sign.
+    //? by calling `provider.sendAndConfirm` it's automatically sign the trasaction 
+    //? by taking the keypair from the provider.
+    // let a = anchor.web3.Keypair.generate();
+    // tx.sign(a);
+
+    let result = await provider.sendAndConfirm(tx);
+    log("Transaction result: ", result);
   })
 
-  it("geting result: ", async () => {
-    let accountDetails = await program.account.answer.fetch(account.publicKey);
-    let sum = accountDetails.res;
-    log("Result: ", sum)
-  })
 
+  //? Sending sol by direct calling system program.
+  it("Sol Transafer (Native)", async () => {
+    let ix = anchor.web3.SystemProgram.transfer({
+      fromPubkey: provider.publicKey,
+      toPubkey: receiver,
+      lamports: 2
+    })
+
+    let tx = new anchor.web3.Transaction();
+    tx.add(ix);
+
+    tx.feePayer = provider.publicKey;
+
+    const blockHashResult = await provider.connection.getLatestBlockhash()
+    tx.recentBlockhash = blockHashResult.blockhash
+
+    let result = await provider.sendAndConfirm(tx);
+    log("Transaction result: ", result);
+  })
 });
 
